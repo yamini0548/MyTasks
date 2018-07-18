d7b53b9e48f4d1d7bdaba6de3a3485e8616ee929
yamini0548 <41290017+yamini0548@users.noreply.github.com>
2018-07-18 11:57:12 +0530

    FileMerge

---
+++ FileMerge.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class FileMerge
+{
+    public static void main(String[] args) throws IOException
+    {
+        
+        PrintWriter pw = new PrintWriter("file.txt");
+        BufferedReader br1 = new BufferedReader(new FileReader("task.txt"));
+        BufferedReader br2 = new BufferedReader(new FileReader("task1.txt"));
+           String line1 = br1.readLine();
+           String line2 = br2.readLine();
+            while (line1 != null || line2 !=null)
+          {
+            if(line1 != null)
+            {
+                pw.println(line1);
+                line1 = br1.readLine();
+            }
+             
+            if(line2 != null)
+            {
+                pw.println(line2);
+                line2 = br2.readLine();
+            }
+        }
+     
+        pw.flush();
+        br1.close();
+        br2.close();
+        pw.close();
+         
+        System.out.println("success");
+    }
+}
