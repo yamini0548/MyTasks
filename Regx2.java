7d566d36c2746e4a40aab783c018e9309d57b95e
yamini0548 <41290017+yamini0548@users.noreply.github.com>
2018-07-19 15:33:36 +0530

    Regx2

---
+++ Regx2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class Regx2
+{
+public static void main(String args[])
+{
+Pattern compile=Pattern.compile("[a-z]{5}[.][a-z]{3}[.][a-z]{2}[.][a-z]{2}");
+Matcher matcher=compile.matcher("gmail.com.co.in");
+while(matcher.find())
+{
+System.out.println(matcher.group());
+}
+}
+}
